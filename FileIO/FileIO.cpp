e11eda513cf1195390ec8590ed0431f867e2e51c
Dave <dmiller@harbrick.com>
2016-05-01 12:09:43 -0700

    File parsing, not elegant.

--- FileIO/FileIO.cpp
+++ FileIO/FileIO.cpp
@@ -4,17 +4,71 @@
 
 using namespace std;
 
+
+float getPrice( string inputLine )
+{
+    auto position = inputLine.find( "$" );
+
+    if( position <= inputLine.size() )
+    {
+        return stod( inputLine.substr( position + 1 ) );
+    }
+
+    return 0;
+}
+
+string getBarCode( string inputLine )
+{
+    auto position = inputLine.find( '$' );
+
+    position -= 1;
+
+    string barCode;
+
+    while( isspace( inputLine[ position ] ) )
+    {
+        --position;
+    }
+
+    while( ! isspace( inputLine[ position ] ) )
+    {
+        barCode.insert( barCode.begin(), inputLine[ position-- ] );
+    }
+
+    return barCode;
+}
+
 int main( int argc, char** argv )
 {
+    cout << "Enter barcode: " << endl;
+
+    string barcodeInput;
+    getline( cin, barcodeInput );
+
     ifstream inputFile( "../FileIO/File.txt" );
 
     if( inputFile.is_open() )
     {
         string inputLine;
+
         while( getline( inputFile, inputLine) )
         {
-            cout << inputLine << endl;
+            auto price = getPrice( inputLine );
+
+            if( price != 0 )
+            {
+                if( getBarCode( inputLine ) == barcodeInput )
+                {
+                    cout << "Price: " << price << endl;
+
+                    inputFile.close();
+
+                    return 0;
+                }
+            }
         }
+
+        cout << "No matching barcode." << endl;
 
         inputFile.close();
     }
