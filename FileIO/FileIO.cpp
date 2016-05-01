3f3af6c265e9fa00d7721f78d930625b61d9f464
Dave <dmiller@harbrick.com>
2016-05-01 11:10:12 -0700

    Read and output file.

--- FileIO/FileIO.cpp
+++ FileIO/FileIO.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+using namespace std;
 
 int main( int argc, char** argv )
 {
+    ifstream inputFile( "../FileIO/File.txt" );
+
+    if( inputFile.is_open() )
+    {
+        string inputLine;
+        while( getline( inputFile, inputLine) )
+        {
+            cout << inputLine << endl;
+        }
+
+        inputFile.close();
+    }
+
     return 0;
 }
