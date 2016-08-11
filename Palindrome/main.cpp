405fed21bf5eaa2f3c22de0a6077710b5b769ffd
Dave <dmiller@harbrick.com>
2016-08-11 11:11:19 -0700

    Code clean.

--- Palindrome/main.cpp
+++ Palindrome/main.cpp
@@ -10,24 +10,39 @@ bool isAPalinDrome( string stringToCheck )
             string{ stringToCheck.rbegin(), stringToCheck.rend() };
 }
 
+void printPalindrome( int index, string stringToPrint )
+{
+    cout << index << ": " << stringToPrint << endl;
+}
+
+bool checkForNextPermutation( string & str )
+{
+    return next_permutation( str.begin(), str.end() );
+}
+
 void findPalindromes( string stringToCheck )
 {
+    auto palindromeNumber = 1;
+
     do
     {
        if( isAPalinDrome( stringToCheck ) )
        {
-           cout << "palindrome: " << stringToCheck << endl;
+           printPalindrome( palindromeNumber++, stringToCheck );
        }
     }
-    while( next_permutation( stringToCheck.begin(), stringToCheck.end() ) );
+    while( checkForNextPermutation( stringToCheck) );
 
     cout << endl;
 }
 
 int main( int argc, char * argv[] )
 {
+    cout << endl;
+
     for( auto index = 1; index < argc; ++index )
     {
+        cout << argv[ index ] << ":" << endl;
         findPalindromes( argv[ index ] );
     }
 }
